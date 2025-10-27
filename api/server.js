e8addf6ae8086b2540e871b78880a74b61817360
DahraFagionato <dahra.fagionato@gmail.com>
2025-10-27 13:43:29 -0300

    .

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./src/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use (cors());
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
